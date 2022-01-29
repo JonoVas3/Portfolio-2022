537e168e6be74e1f7245a6904bf00a4f027de6b9
JonoVas3 <jonv.designs@gmail.com>
2022-01-29 12:07:13 -0500

    Version 2.2022.1 Update

---
+++ src/component/ProjectModal/ProjectModal.js
@@ -0,0 +1,80 @@
+import { Modal, Typography, Button, Paper, Grid } from "@mui/material";
+import { Box } from "@mui/system";
+import { useContext, useState } from "react";
+import { projectModalOpenContext, projectModalObjContext } from "../Home";
+import hvTechPs from '../../assets/synopsis-hv_tech.jpg'
+import emperorsPS from '../../assets/synopsis-emperors.jpg'
+import eclsPs from '../../assets/synopsis-ecls.jpg'
+import voidChatPs from '../../assets/synopsis-voidchat.jpg'
+import pcHainesPs from '../../assets/synopsis-pc_haines.jpg'
+
+function ProjectModal(props) {
+
+    // EXAMPLE OF STATEFUL COMPONTENT - DOES NOT HAVE PROPS
+
+    const { projectModalOpen, setProjectModalOpen } = useContext(projectModalOpenContext);
+    // uses 'projectModalOpenContext' from Home
+    const { projectModalObj, setProjectModalObj } = useContext(projectModalObjContext);
+    const [value, setValue] = useState("false");
+    // sets 'value' to 'setValue' is otherwise it is set to false 
+    console.log(projectModalObj)
+
+    let imagePs;
+    switch (projectModalObj.title) {
+        case "HV Tech Solutions":
+            imagePs = hvTechPs;
+            break;
+        case "Emperors E-Sports":
+            imagePs = emperorsPS;
+            break;
+        case "ECLS Inc":
+            imagePs = eclsPs;
+            break;
+        case "VoidChat":
+            imagePs = voidChatPs;
+            break;
+        case "P.C. Haines":
+            imagePs = pcHainesPs;
+            break;
+    }
+
+    return (
+        <Modal
+            open={projectModalOpen}
+            aria-labelledby="project-modal-label"
+            aria-describedby="project-modal-description"
+        >
+            <Grid container sx={{ maxWidth:'800px', margin:'50px auto 0', border:'none', borderRadius:'5px', overflow:'hidden', backgroundColor:'#080808' }}>
+                {
+                // grabs elements from project Modal 
+                }
+                <Grid item xs={12} sm={8} md={8} lg={8} sx={{ overflow:'scroll', height:'810px', backgroundColor:'#080808' }}>
+                        <img src={imagePs} title={props.title} alt={props.altText} width='100%'/>
+                </Grid>
+                <Grid item xs={12} sm={4} md={4} lg={4} sx={{ padding:'30px 0 0 20px', backgroundColor:'#080808' }}>
+                    <Typography id="project-modal-title" variant="h6" component="h2" sx={{ color:'#c71010', fontFamily: 'Raleway', fontWeight:'900', textTransform: 'uppercase' }} >
+                        {projectModalObj.title}
+                    </Typography>
+                    <Typography id="project-modal-type" sx={{ fontFamily: 'Raleway', fontWeight:'700', color:'#c9c9c9' }} >
+                        {projectModalObj.type}
+                    </Typography>
+                    <Typography id="project-modal-info" sx={{ fontFamily: 'Raleway', fontWeight:'300', color:'#c9c9c9', margin:'10px 0 0' }} >
+                        {projectModalObj.info}
+                    </Typography>
+                    <Button onClick={() => setProjectModalOpen(false)} sx={{ margin:'20px auto 0', bgcolor:'#c71010', color:'#ffffff' }}>
+                        Close
+                    </Button>
+                </Grid>
+
+            </Grid>
+        </Modal>
+
+    )
+
+
+};
+
+
+
+
+export default ProjectModal;
---
+++ src/data/Tools.js
@@ -0,0 +1,75 @@
+// Title and Alt Text for Tool Icons, referenced in ToolIcon.js
+
+const photoshop = {
+    url: '../../assets/icon_photoshop.png',
+    alt_text: 'Photoshop',
+    title: 'Photoshop'
+}
+
+const illustrator = {
+    url: 'https://cdn3.iconfinder.com/data/icons/adobe-family-software/512/Adobe-01-512.png',
+    alt_text: 'Illustrator',
+    title: 'Illustrator'
+}
+
+const xd = {
+    url: 'https://cdn3.iconfinder.com/data/icons/adobe-family-software/512/Adobe-35-512.png',
+    alt_text: 'Adobe XD',
+    title: 'Adobe XD'
+}
+
+const indesign = {
+    url: 'https://cdn3.iconfinder.com/data/icons/adobe-family-software/512/Adobe-20-512.png',
+    alt_text: 'Indesign',
+    title: 'Indesign'
+}
+
+const premierePro = {
+    url: 'https://cdn3.iconfinder.com/data/icons/adobe-family-software/512/Adobe-26-512.png',
+    alt_text: 'Premiere Pro',
+    title: 'Premiere Pro'
+}
+
+const afterEffects = {
+    url: 'https://cdn3.iconfinder.com/data/icons/adobe-family-software/512/Adobe-03-512.png',
+    alt_text: 'After Effects',
+    title: 'After Effects'
+}
+
+const html5 = {
+    url: 'https://cdn1.iconfinder.com/data/icons/logotypes/32/badge-html-5-256.png',
+    alt_text: 'HTML 5',
+    title: 'HTML 5'
+}
+
+const css3 = {
+    url: 'https://cdn1.iconfinder.com/data/icons/logotypes/32/badge-css-3-256.png',
+    alt_text: 'CSS 3',
+    title: 'CSS 3'
+}
+
+const sonyAlpha = {
+    url: 'https://seeklogo.com/images/S/Sony_Alpha-logo-3BA7DFA79B-seeklogo.com.png',
+    alt_text: 'Sony Alpha',
+    title: 'Sony Alpha'
+}
+
+const javascript = {
+    url: 'https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/233_Node_Js_logo-512.png',
+    alt_text: 'Javascript',
+    title: 'Javascript'
+}
+
+const react = {
+    url: 'https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png',
+    alt_text: 'React',
+    title: 'React'
+}
+
+const materialUi = {
+    url: 'https://v4.mui.com/static/logo.png',
+    alt_text: 'Material UI',
+    title: 'Material UI'
+}
+
+export const toolArray = [photoshop, illustrator, xd, indesign, premierePro, afterEffects, html5, css3, sonyAlpha, javascript, react, materialUi]
