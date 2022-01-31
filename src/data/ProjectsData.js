7a9326bf5434cb8e3928f68f6fe0522880f2e0f9
JonoVas3 <jonv.designs@gmail.com>
2022-01-30 21:34:13 -0500

    update to Projects Data

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -1,43 +1,43 @@
 const hvTech = {
     alt_text: 'HV Tech',
     title: 'HV Tech Solutions',
-    type: 'Branding, Website',
-    info: 'This is a test of HV Tech',
+    type: 'Branding, Website, Print',
+    info: 'HV Tech Solutions is a verteran-owned and operated business, whose main product was a special heat-resistant paint, so we focused on those elements with their branding.',
 }
 
 const emperors = {
     alt_text: 'Emperors E-Sports',
     title: 'Emperors E-Sports',
-    type: 'Branding, Website',
-    info: 'This is a test of HV Tech',
+    type: 'Branding',
+    info: 'The Emperors E-Sports brand was a project I worked on to practice my illustration skills, and I later expanded to cover a range of branding for.',
 }
 
 const ecls = {
     alt_text: 'ECLS Inc',
     title: 'ECLS Inc',
     type: 'Branding, Website',
-    info: 'This is a test of HV Tech',
+    info: 'ECLS Inc. was a compnay who wanted their branding to catupre the pioneering spirit in land planners and surveyors.',
 }
 
 const pcHaines = {
     alt_text: 'P.C. Haines',
     title: 'P.C. Haines',
     type: 'Branding, Website',
-    info: 'This is a test of HV Tech',
+    info: 'P.C. Haines is a client I worked with while I was with Textivia. I created thier branding and their Website, which the requested to have a very modern aesthetic.',
 }
 
 const medPak = {
     alt_text: 'Medical Packaging Inc',
     title: 'Medical Packaging Inc',
     type: 'Branding, Website',
-    info: 'This is a test of HV Tech',
+    info: 'Medical Packaging Inc. or MedPak for short, is a company who I designed a website for in 2016. I redesigned it in 2022.',
 }
 
 const voidChat = {
     alt_text: 'VoidChat',
     title: 'VoidChat',
     type: 'Branding, Website',
-    info: 'This is a test of HV Tech',
+    info: 'VoidChat was a project I worked with some friends on developing a anonymous local chat app with, in which I designed and developed the branding and commercial website.',
 }
 
 
